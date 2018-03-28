c5c7884454d34d5f428711d2d24b6504efaf68b7
Shawn(Shaofeng) Jiang <sf.jiang.ca@gmail.com>
2018-03-28 15:02:03 -0400

    Add files via upload

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+class App extends React.Component {
+  public render(): JSX.Element {
+    return (
+      <div>
+          <h1>Welcome to React</h1>
+        <p>
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export {App};
