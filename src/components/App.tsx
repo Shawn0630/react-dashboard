b04880d164469039889e6743a7147e81e530227a
Shaofeng Jiang <sf.jiang.ca@gmail.com>
2018-03-29 09:11:36 -0400

    Fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
+import * as styles from "../styles/app.scss";
 
 class App extends React.Component {
   public render(): JSX.Element {
     return (
       <div>
-          <h1>Welcome to React</h1>
+        <h1 className={styles.appHeader}>Welcome to React</h1>
         <p>
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
