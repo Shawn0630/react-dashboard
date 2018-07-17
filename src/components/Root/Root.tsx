43623d77caa5a08c024e15b2db33515b63064454
Shaofeng Jiang <sf.jiang.ca@gmail.com>
2018-07-17 00:55:18 -0400

    Fixed

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
-import * as styles from "../../styles/shared.scss";
 import {ScatterChart} from "../D3/ScatterChart";
 import {App} from "../App";
 
