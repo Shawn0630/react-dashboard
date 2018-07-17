1aa3168352f61781a98360e5fbf4e13d8d2909fa
Shaofeng Jiang <sf.jiang.ca@gmail.com>
2018-07-17 00:26:31 -0400

    Update

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -2,12 +2,14 @@ import * as React from "react";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import * as styles from "../../styles/shared.scss";
 import {ScatterChart} from "../D3/ScatterChart";
+import {App} from "../App";
 
 class Root extends React.PureComponent<{}> {
 
     public render(): JSX.Element {
-        return <Router>
+        return <Router basename={process.env.PUBLIC_URL}>
             <div id="content">
+                <Route path="/" exact={true} component={App}/>
                 <Route path="/scatter" exact={true} component={ScatterChart}/>
             </div>
         </Router>;
